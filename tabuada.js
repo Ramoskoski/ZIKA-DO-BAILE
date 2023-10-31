3d3b64584e6a98cc6b5651d8fa975993f4e0f491
Ramoskoski <126572347+Ramoskoski@users.noreply.github.com>
2023-10-31 12:28:54 -0300

    Add files via upload

--- tabuada.js
+++ tabuada.js
@@ -1,31 +1,15 @@
-const form = document.querySelector("form")
-const resp = document.querySelector("#outResp")
-const resp1 = document.querySelector("#outResp1")
+const frm = document.querySelector("form")
+const resp =document.querySelector("pre")
 
 //ouvinte
-form.addEventListener("submit", (e)=>{
+frm.addEventListener("submit", (e) =>{
     e.preventDefault()
 
-    const ladoA = Number(form.inLadoA.value)
-    const ladoB = Number(form.inLadoB.value)
-    const ladoC = Number(form.inLadoC.value)
-
-
-    if((ladoA > ladoB + ladoC) || (ladoB > ladoA + ladoC) || (ladoC > ladoA + ladoB)){
-        alert("Essas medidas não podem formar um triângulo. Um dos lados não pode ser maior que a soma dos outros dois")
-        form.inLadoA.focus()
-        form.inLadoB.focus()
-        form.inLadoC.focus()
-        return
+    const numero = Number(frm.inNumero.value)
+    let resposta = ""     //variável do tipo String, para concatenar a resposta
+//cria um laço de repetição
+    for(let i = 1; i <= 10; i++){
+        resposta = resposta + numero + " x " + i + " = " + (numero * i)+ "\n"
     }
-
-    resp.innerText = "Lados podem formar um triângulo"
-
-    if(ladoA == ladoB && ladoA == ladoC){
-        resp1.innerText = `Tipo: Equilátero`
-    } else if((ladoA == ladoB) || (ladoA == ladoC) || (ladoB == ladoC)){
-        resp1.innerText = `Tipo: Isóceles`
-    } else{
-        resp1.innerText = `Tipo: Escaleno`
-    }
-})    
+    resp.innerText = resposta
+})
